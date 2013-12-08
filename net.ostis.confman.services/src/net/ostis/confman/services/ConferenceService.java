af53f1b1ac41053b028aa90179ecf3a300553083
Stas Ter <lgsferry@gmail.com>
2013-12-08 23:20:07 +0300

    [issue]: Conference data Maintenance
    [url]: https://github.com/Ivan-Zhukau/conforg-desktop/issues/30
    
    [In commit]:
    1) Updated ConferenceService. Added possibility to add/update/remove
    sections/reports.
    
    Issue #30

--- net.ostis.confman.services/src/net/ostis/confman/services/ConferenceService.java
+++ net.ostis.confman.services/src/net/ostis/confman/services/ConferenceService.java
@@ -3,9 +3,19 @@ package net.ostis.confman.services;
 import java.util.List;
 
 import net.ostis.confman.services.common.model.Conference;
+import net.ostis.confman.services.common.model.Report;
+import net.ostis.confman.services.common.model.Section;
 
 public interface ConferenceService {
 
     List<Conference> getConferences();
+    
+    void addSection(Conference conference, Section section);
+    
+    void addReport(Section section, Report report);
 
+    void moveReport(Report report, Section from, Section to);
+    
+    void deleteReport(Report report);
+    
 }
--- net.ostis.confman.services/src/net/ostis/confman/services/ConferenceServiceImpl.java
+++ net.ostis.confman.services/src/net/ostis/confman/services/ConferenceServiceImpl.java
@@ -2,18 +2,23 @@ package net.ostis.confman.services;
 
 import java.util.List;
 
+import net.ostis.confman.model.datastore.StorageProvider;
 import net.ostis.confman.model.datastore.local.convert.ConverterFromStorageProvider;
 import net.ostis.confman.services.common.model.Conference;
 import net.ostis.confman.services.common.model.FullModel;
+import net.ostis.confman.services.common.model.Report;
+import net.ostis.confman.services.common.model.Section;
 
 class ConferenceServiceImpl implements ConferenceService {
 
-    List<Conference> conferences;
+    private List<Conference> conferences;
+    
+    private FullModel model;
 
     public ConferenceServiceImpl() {
 
         final ConverterFromStorageProvider converter = new ConverterFromStorageProvider();
-        final FullModel model = converter.convertData();
+        model = converter.convertData();
         this.conferences = model.getConferences();
     }
 
@@ -21,5 +26,43 @@ class ConferenceServiceImpl implements ConferenceService {
     public List<Conference> getConferences() {
 
         return this.conferences;
+    }
+
+    @Override
+    public void addSection(Conference conference, Section section) {
+
+        conference.getSections().add(section);
+        fireData();
+    }
+
+    @Override
+    public void addReport(Section section, Report report) {
+
+        section.getConference().getReports().add(report);
+        section.getReports().add(report);
+        fireData();
+    }
+
+    @Override
+    public void moveReport(Report report, Section from, Section to) {
+
+        from.getReports().remove(report);
+        to.getReports().add(report);
+        report.setSection(to);
+        fireData();
+    }
+
+    @Override
+    public void deleteReport(Report report) {
+
+        Section section = report.getSection();
+        section.getConference().getReports().remove(report);
+        section.getReports().remove(report);
+        fireData();
+    }
+    
+    private void fireData() {
+        StorageProvider storageProvider = StorageProvider.getInstance();
+        storageProvider.persist(model);
     }
 }
