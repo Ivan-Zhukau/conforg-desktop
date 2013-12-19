3f4a2ea16fb7bd31f329882153bf8d4c875606a9
Dmitry <Kosmich27@mail.ru>
2013-12-19 21:30:27 +0300

    Added item menu, eliminated possibility of NPE

--- net.ostis.confman.services.common/src/net/ostis/confman/services/common/model/SectionSettings.java
+++ net.ostis.confman.services.common/src/net/ostis/confman/services/common/model/SectionSettings.java
@@ -19,6 +19,16 @@ public class SectionSettings {
     public SectionSettings() {
 
         super();
+        setAllNull();
+    }
+
+    private void setAllNull() {
+        this.breakTime = 0;
+        this.chairmanTime = 0;
+        this.coffeeBreakNumber = 0;
+        this.coffeeBreakTime = 0;
+        this.plenaryReportTime = 0;
+        this.reportTime = 0;        
     }
 
     public Section getSection() {
