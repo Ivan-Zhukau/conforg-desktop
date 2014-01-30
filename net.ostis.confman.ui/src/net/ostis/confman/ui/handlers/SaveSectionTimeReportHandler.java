471543862aaf1226fc93384b53cf3cc9121f695e
Dmitry <Kosmich27@mail.ru>
2014-01-30 16:56:38 +0300

    issue: Report Module: Section Time Report: Provide corresponding menu and tool bar button which executes report generation url: https://github.com/Ivan-Zhukau/conforg-desktop/issues/43 In commit: 1) Clean up 2) Added localization of menu item.

--- net.ostis.confman.ui/src/net/ostis/confman/ui/handlers/SaveSectionTimeReportHandler.java
+++ net.ostis.confman.ui/src/net/ostis/confman/ui/handlers/SaveSectionTimeReportHandler.java
@@ -10,19 +10,37 @@
  *******************************************************************************/
 package net.ostis.confman.ui.handlers;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
 import javax.inject.Named;
+
+import net.ostis.confman.services.ScheduleServiceImpl;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.services.IServiceConstants;
-import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
 
-public class SaveReportHandler {
+public class SaveSectionTimeReportHandler {
 
     @Execute
     public void execute(@Named(IServiceConstants.ACTIVE_SHELL) final Shell shell) {
 
-        MessageDialog.openInformation(shell, "About",
-                "Eclipse 4 Application example.");
+        final FileDialog dialog = new FileDialog(shell, SWT.SAVE);
+        dialog.setFilterNames(new String[] { "xlsx", "xls" });
+        dialog.setFilterExtensions(new String[] { "*.xlsx", "*.xls" });
+        dialog.setFileName("Section_schedule.xlsx");
+        final String filePath = dialog.open();
+        if (filePath != null) {
+            try {
+                new ScheduleServiceImpl().save(new FileOutputStream(new File(
+                        filePath)));
+            } catch (final FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
