e024e9e82a38e90c54e343836a09c48a0f66e1e5
kfs <lgsferry@gmail.com>
2014-11-09 20:18:36 +0300

    #84 participant arrival perspective added

---
+++ net.ostis.confman.ui/src/net/ostis/confman/ui/parts/arrival/ParticipantArrivalView.java
@@ -0,0 +1,33 @@
+package net.ostis.confman.ui.parts.arrival;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+import org.eclipse.e4.core.services.events.IEventBroker;
+import org.eclipse.e4.ui.workbench.modeling.EPartService;
+import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.swt.widgets.Composite;
+
+
+public class ParticipantArrivalView {
+
+    @Inject
+    private ESelectionService selectionService;
+
+    @Inject
+    private EPartService      partService;
+
+    @Inject
+    private IEventBroker      eventBroker;
+
+    public ParticipantArrivalView() {
+        super();
+    }
+
+    @PostConstruct
+    public void createComposite(final Composite parent) {
+
+        //initTreeViewer(parent);
+    }
+
+}
