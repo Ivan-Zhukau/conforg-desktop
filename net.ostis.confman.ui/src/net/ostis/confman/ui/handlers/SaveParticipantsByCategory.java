c397210ad441dc7053a0e9ca39c7303854629ad0
kfs <lgsferry@gmail.com>
2014-12-02 02:28:10 +0300

    select a conference dialog added

--- net.ostis.confman.ui/src/net/ostis/confman/ui/handlers/SaveParticipantsByCategory.java
+++ net.ostis.confman.ui/src/net/ostis/confman/ui/handlers/SaveParticipantsByCategory.java
@@ -8,9 +8,12 @@ import javax.inject.Named;
 
 import net.ostis.confman.services.ReportService;
 import net.ostis.confman.services.ServiceLocator;
+import net.ostis.confman.services.common.model.Conference;
+import net.ostis.confman.ui.conference.SelectConferenceDialog;
 
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.services.IServiceConstants;
+import org.eclipse.jface.window.Window;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.FileDialog;
 import org.eclipse.swt.widgets.Shell;
@@ -19,6 +22,18 @@ public class SaveParticipantsByCategory {
 
     @Execute
     public void execute(@Named(IServiceConstants.ACTIVE_SHELL) final Shell shell) {
+
+        SelectConferenceDialog dialog = new SelectConferenceDialog(shell);
+        dialog.create();
+        if (dialog.open() == Window.OK) {
+            final Conference selectedConf = dialog.getSelectedConf();
+            if (selectedConf != null) {
+                generateReport(shell, selectedConf);
+            }
+        }
+    }
+
+    private void generateReport(Shell shell, Conference selectedConf) {
 
         final FileDialog dialog = new FileDialog(shell, SWT.SAVE);
         dialog.setFilterNames(new String[] { "xlsx", "xls" });
