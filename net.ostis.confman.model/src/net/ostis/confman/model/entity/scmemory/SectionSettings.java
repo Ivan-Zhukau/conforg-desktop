b911254f5934a0dbf686206bc2bdd782d82effef
kfs <lgsferry@gmail.com>
2015-05-09 23:10:08 +0300

    model clean up

--- net.ostis.confman.model/src/net/ostis/confman/model/entity/scmemory/SectionSettings.java
+++ net.ostis.confman.model/src/net/ostis/confman/model/entity/scmemory/SectionSettings.java
@@ -1,30 +1,110 @@
 package net.ostis.confman.model.entity.scmemory;
 
-import java.util.List;
+import java.util.UUID;
 
-public class SectionSettings extends BaseEntity {
+public class SectionSettings {
 
-    private List<SectionBreaks> sectionBreaks;
+    private UUID sectionId;
+
+    private int  chairmanTime;
+
+    private int  reportTime;
+
+    private int  plenaryReportTime;
+
+    private int  breakTime;
+
+    private int  coffeeBreaksNum;
+
+    private int  coffeeBreaksTime;
 
     public SectionSettings() {
 
         super();
     }
 
-    public SectionSettings(List<SectionBreaks> sectionBreaks) {
+    public SectionSettings(UUID sectionId, int chairmanTime, int reportTime,
+            int plenaryReportTime, int breakTime, int coffeeBreaksNum,
+            int coffeeBreaksTime) {
 
         super();
-        this.sectionBreaks = sectionBreaks;
+        this.sectionId = sectionId;
+        this.chairmanTime = chairmanTime;
+        this.reportTime = reportTime;
+        this.plenaryReportTime = plenaryReportTime;
+        this.breakTime = breakTime;
+        this.coffeeBreaksNum = coffeeBreaksNum;
+        this.coffeeBreaksTime = coffeeBreaksTime;
     }
 
-    public List<SectionBreaks> getSectionBreaks() {
+    public UUID getSectionId() {
 
-        return this.sectionBreaks;
+        return this.sectionId;
     }
 
-    public void setSectionBreaks(final List<SectionBreaks> sectionBreaks) {
+    public void setSectionId(final UUID sectionId) {
 
-        this.sectionBreaks = sectionBreaks;
+        this.sectionId = sectionId;
+    }
+
+    public int getChairmanTime() {
+
+        return this.chairmanTime;
+    }
+
+    public void setChairmanTime(final int chairmanTime) {
+
+        this.chairmanTime = chairmanTime;
+    }
+
+    public int getReportTime() {
+
+        return this.reportTime;
+    }
+
+    public void setReportTime(final int reportTime) {
+
+        this.reportTime = reportTime;
+    }
+
+    public int getPlenaryReportTime() {
+
+        return this.plenaryReportTime;
+    }
+
+    public void setPlenaryReportTime(final int plenaryReportTime) {
+
+        this.plenaryReportTime = plenaryReportTime;
+    }
+
+    public int getBreakTime() {
+
+        return this.breakTime;
+    }
+
+    public void setBreakTime(final int breakTime) {
+
+        this.breakTime = breakTime;
+    }
+
+    public int getCoffeeBreakNumber() {
+
+        return this.coffeeBreaksNum;
+    }
+
+    public void setCoffeeBreaksNum(final int coffeeBreaksNum) {
+
+        this.coffeeBreaksNum = coffeeBreaksNum;
+    }
+
+    public int getCoffeeBreakTime() {
+
+        return this.coffeeBreaksTime;
+    }
+
+    public void setCoffeeBreaksTime(final int coffeeBreaksTime) {
+
+        this.coffeeBreaksTime = coffeeBreaksTime;
     }
 
 }
